9217a5933312064a6419a76359233fbbf52a92c6
Sai Harshit B <sai.harshitbalaji@gmail.com>
2025-06-21 13:02:16 +0530

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Welcome to Sai Harshit's Portfolio! This is a simple Vite + React app.'
       </p>
     </>
   )
