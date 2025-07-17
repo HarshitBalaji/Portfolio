d81c144741f0be049a3a09b8c9cd20b882e53d23
Sai Harshit B <sai.harshitbalaji@gmail.com>
2025-07-17 10:45:29 +0530

    Changed style of Other pages and navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,22 @@ import Logo from '../assets/logo.png'
 function Navbar() {
   const [count, setCount] = useState(0)
   const navigate = useNavigate();
-  const start = (
-    <div className="flex align-items-center" id='logo' style={{ marginRight: '0%' }} onClick={() => navigate('/')}>
-      <img src={Logo} alt="logo" height="45" className="mr-2" />
-      <span style={{ fontSize: '1.5rem', fontWeight: 'bold', height: '100%',display: 'inline-block', lineHeight: '1' }}>Sai Harshit B</span>
-    </div>
-  );
-  const items = [
-            { label: 'CV', icon: 'pi pi-fw pi-history', command: () => navigate('/app/about') },
-            { label: 'Works', icon: 'pi pi-fw pi-cloud-upload', command: () => navigate('/app/work') },
-            { label: 'Contact', icon: 'pi pi-fw pi-users', command: () => navigate('/app/contact') },
-  ]
   return (
-      <Menubar model={items} start={start} pt={{ menu: { style:{marginLeft:'60%'} } }} />
+      <div className="navbar">
+        <span style={{ display: 'flex', alignItems: 'center' }} onClick={() => navigate('/')}>
+          <img src={Logo} alt="logo" height="50" />
+          <span style={{ fontSize: '1.5rem', fontFamily: 'Bropella', fontWeight: 'bold', height: '100%', display: 'inline-block', lineHeight: '1' }}>Sai Harshit B</span>
+        </span>
+        <span>
+          <span className="options" onClick={() => {navigate('/')}}>Home</span>
+          <span className="options" onClick={() => {navigate('/app/about')}}>About Me</span>
+          <span className="options" onClick={() => {navigate('/app/work')}}>View my Work</span>
+          <span className="options" onClick={() => {navigate('/app/contact')}}>Contact</span>
+        </span>
+      </div>
   )
 }
 
 export default Navbar
+
+{/*<Menubar model={items} start={start} pt={{ menu: { style:{marginLeft:'60%'} } }} />*/}
