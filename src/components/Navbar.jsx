e79884b8f2d69f3aa29532f37e2674d1b09b7029
Sai Harshit B <sai.harshitbalaji@gmail.com>
2025-07-17 13:16:53 +0530

    fixed page title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import ReactDOM from "react-dom/client";
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Menubar } from 'primereact/menubar';
 import Logo from '../assets/logo.png'
 
 function Navbar() {
-  const [count, setCount] = useState(0)
+  const pageTitles = {
+    '/app/about': 'About Me',
+    '/app/work': 'My Work',
+    '/app/contact': 'Contact',
+  };
   const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const pageTitle = pageTitles[currentPath] || " ";
+
   return (
       <div className="navbar">
         <span style={{ display: 'flex', alignItems: 'center' }}>
           <img className="logoButton" src={Logo} alt="logo" height="50" onClick={() => navigate('/')} />
-          <span style={{ marginLeft:"1rem", fontSize: '2rem', fontFamily: 'Bropella' }}>Sai Harshit B</span>
+          <span id="pageTitle" style={{ marginLeft:"1rem", fontSize: '2rem', fontFamily: 'Bropella' }}>{pageTitle}</span>
         </span>
         <span>
           <span className="options" onClick={() => {navigate('/')}}>Home</span>
